f29ca19158de530f9d102d741111b43505218005
MaximAdler <orelmax@i.ua>
2016-08-11 23:04:50 +0300

    add Local Storage

--- js/MainController.js
+++ js/MainController.js
@@ -2,54 +2,49 @@ var app = angular.module('app');
 
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 
-//items
+    //items
 
-    $http.get('./data/rows.json').success(function(data) {
+    // $http.get('./data/rows.json').success(function(data) {
+    //     $scope.rows = data;
+    // });
+
+    $scope.saved = localStorage.getItem('rows');
+    $scope.rows = (localStorage.getItem('rows') !== null) ? JSON.parse($scope.saved) : $http.get('./data/rows.json').success(function(data) {
         $scope.rows = data;
     });
+    localStorage.setItem('rows', JSON.stringify($scope.rows));
+
     $scope.addRow = function() {
-      if(this.name){
-        $scope.rows.push({'name': $scope.name});
-        $scope.name = '';
-      }
+        if (this.name) {
+            $scope.rows.push({
+                name: $scope.name
+            });
+            $scope.name = '';
+
+            localStorage.setItem('rows', JSON.stringify($scope.rows));
+        }
     };
 
-    // $scope.removeRow = function(name){
-    // 		var index = -1;
-    // 		var arr = eval( $scope.rows );
-    // 		for( var i = 0; i < arr.length; i++ ) {
-    // 			if( arr[i].name === name ) {
-    // 				index = i;
-    // 				break;
-    // 			}
-    // 		}
-    // 		if( index === -1 ) {
-    // 			alert( "Something gone wrong" );
-    // 		}
-    // 		$scope.rows.splice( index, 1 );
-    // 	};
     $scope.removeRow = function(idx) {
         $scope.rows.splice(idx, 1);
+        localStorage.setItem('rows', JSON.stringify($scope.rows));
     };
 
-//comments
 
-    $http.get("./data/comments.json").success(function(data){
-      $scope.comment = data;
+
+    //comments
+
+    $http.get("./data/comments.json").success(function(data) {
+        $scope.comment = data;
     })
-    $scope.addComment = function(){
-      if(this.txtcomment){
-        $scope.comment.push({"text":$scope.txtcomment});
-        $scope.txtcomment = '';
-      }
+    $scope.addComment = function() {
+        if (this.txtcomment) {
+            $scope.comment.push({
+                "text": $scope.txtcomment
+            });
+            $scope.txtcomment = '';
+        }
     }
-
-
-
-
-
-
-
 
 
 }]);
