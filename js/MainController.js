3eb9075bdb7329948496558faeef52a2d4bdae37
MaximAdler <orelmax@i.ua>
2016-08-11 19:19:38 +0300

    fix bugs

--- js/MainController.js
+++ js/MainController.js
@@ -1,47 +1,55 @@
 var app = angular.module('app');
 
-app.controller('MainController',['$scope',function($scope){
-$scope.name='Type name here...';
-$scope.rows = ['First item with custom name','Second  item is active'];
-$scope.counter = 1;
-$scope.addRow = function(){
-	$scope.rows.push($scope.name + ' '+$scope.counter);
-  $scope.counter++;
-};
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+
+//items
+
+    $http.get('./data/rows.json').success(function(data) {
+        $scope.rows = data;
+    });
+    $scope.addRow = function() {
+      if(this.name){
+        $scope.rows.push({'name': $scope.name});
+        $scope.name = '';
+      }
+    };
+
+    // $scope.removeRow = function(name){
+    // 		var index = -1;
+    // 		var arr = eval( $scope.rows );
+    // 		for( var i = 0; i < arr.length; i++ ) {
+    // 			if( arr[i].name === name ) {
+    // 				index = i;
+    // 				break;
+    // 			}
+    // 		}
+    // 		if( index === -1 ) {
+    // 			alert( "Something gone wrong" );
+    // 		}
+    // 		$scope.rows.splice( index, 1 );
+    // 	};
+    $scope.removeRow = function(idx) {
+        $scope.rows.splice(idx, 1);
+    };
+
+//comments
+
+    $http.get("./data/comments.json").success(function(data){
+      $scope.comment = data;
+    })
+    $scope.addComment = function(){
+      if(this.txtcomment){
+        $scope.comment.push({"text":$scope.txtcomment});
+        $scope.txtcomment = '';
+      }
+    }
 
 
 
-$scope.removeRow = function(name){
-		var index = -1;
-		var arr = eval( $scope.rows );
-		for( var i = 0; i < arr.length; i++ ) {
-			if( arr[i].name === name ) {
-				index = i;
-				break;
-			}
-		}
-		if( index === -1 ) {
-			alert( "Something gone wrong" );
-		}
-		$scope.rows.splice( index, 1 );
-	};
-
-}])
 
 
 
 
 
-// angular.module('MyApp', [])
-// .controller('MainController', [ '$scope', function($scope) {
-//
-//   $scope.rows = ['Row 1', 'Row 2'];
-//
-//   $scope.counter = 3;
-//
-//   $scope.addRow = function() {
-//
-//     $scope.rows.push('Row ' + $scope.counter);
-//     $scope.counter++;
-//   }
-// }]);
+
+}]);
