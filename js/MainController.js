11499cd45256e412e2cc5eca67823830d1d53fb3
MaximAdler <orelmax@i.ua>
2016-08-12 21:46:30 +0300

    fix all, Solv Task

--- js/MainController.js
+++ js/MainController.js
@@ -1,75 +1,55 @@
 var app = angular.module('app');
 
-app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+app.controller('MainController', ['$scope', '$window', '$http', '$compile', function($scope, $window, $http, $compile) {
 
     //items
 
-    // $http.get('./data/rows.json').success(function(data) {
-    //     $scope.rows = data;
-    // });
+    $scope.rows = get();
+    $scope.$watch('rows', function(newValue, oldValue) {
+        set(newValue);
+    }, true)
 
-    $scope.saved = localStorage.getItem('rows');
-    $scope.rows = (localStorage.getItem('rows') !== null) ? JSON.parse($scope.saved) : $http.get('./data/rows.json').success(function(data) {
-        $scope.rows = data;
-    });
-    localStorage.setItem('rows', JSON.stringify($scope.rows));
-
-    $scope.rowsIndex = 0;
-
-    $scope.index = function(id) {
-       $scope.rowsIndex = id;
-      console.log(id);
+    $scope.addRow = function(name) {
+        if (this.row.name) {
+            $scope.rows.push({
+                name: $scope.row.name,
+                comments: []
+            })
+            $scope.row.name = '';
+        }
     }
 
-    $scope.addRow = function() {
-        if (this.name) {
-            $scope.rows.push({
-                name: $scope.name,
-                id: $scope.rows.lenght + 1
-            });
-            $scope.name = '';
-
-            localStorage.setItem('rows', JSON.stringify($scope.rows));
-        }
+    $scope.removeRow = function(i) {
+        $scope.rows.splice(i, 1);
     };
 
-    $scope.removeRow = function(idx) {
-        $scope.rows.splice(idx, 1);
-        localStorage.setItem('rows', JSON.stringify($scope.rows));
-    };
-
-//Select Row
-
-
-$scope.showDetails = true;
-    // $scope.selectedRow = null;
-    //
-    // $scope.getDetails = function(app) {
-    //     $scope.selectedRow = app;
-    // };
-
-    //comments
-
-
-
-
-
-
-$scope.savedCom = localStorage.getItem('comment');
-$scope.comment = (localStorage.getItem('comment') !== null) ? JSON.parse($scope.savedCom) : $http.get('./data/comments.json').success(function(data) {
-    $scope.comment= data;
-});
-localStorage.setItem('comment', JSON.stringify($scope.comment));
+    //Comments
 
     $scope.addComment = function() {
-        if (this.txtcomment) {
-            $scope.comment.push({
-                text: $scope.txtcomment
-            });
-            $scope.txtcomment = '';
-            localStorage.setItem('comment',JSON.stringify($scope.comment));
+        if ($scope.currentRow) {
+            if (this.txtcomment) {
+                $scope.currentRow.comments.push($scope.txtcomment)
+                $scope.txtcomment = '';
+            }
         }
     }
 
+    $scope.onComments = function(i) {
+        $scope.rowIndex = i;
+        $scope.currentRow = $scope.rows[i];
+    }
+
+    //localStorage
+
+    function get() {
+        if (!$window.localStorage.rows) {
+            $window.localStorage.rows = JSON.stringify([]);
+        }
+        return JSON.parse($window.localStorage.rows);
+    }
+
+    function set(rows) {
+        $window.localStorage.rows = JSON.stringify(rows);
+    }
 
 }]);
--- js/app.js
+++ js/app.js
@@ -1,8 +1,1 @@
 var app = angular.module('app',[]);
-// app.config(['$routeProvider',function($routeProvider){
-//   $routeProvider
-//   .when('/',{
-//     templateUrl: 'pages/comments.html',
-//     controller: 'MainController'
-//   });
-// }])
